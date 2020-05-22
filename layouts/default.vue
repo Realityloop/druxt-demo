f4adb0bcdf998fd8f736bf6195faeb5b9ae613f1
Stuart Clark <stuart@realityloop.com>
2020-05-22 18:14:21 +1000

    [#1] Initial proof of concept.

---
+++ layouts/default.vue
@@ -0,0 +1,85 @@
+<template>
+  <b-container>
+    <b-row>
+      <b-col>
+        <druxt-breadcrumb component="b-breadcrumb" />
+      </b-col>
+    </b-row>
+    <b-row>
+      <b-col cols="3">
+        <druxt-menu component="b-nav" :depth="2">
+          <template #item="{ item: { entity } }">
+            <b-nav-item :to="entity.attributes.url">
+              {{ entity.attributes.title }}
+            </b-nav-item>
+          </template>
+
+          <template #parent="{ item: { entity, children }}">
+            <b-nav-item-dropdown :text="entity.attributes.title">
+              <druxt-menu-item :item="{ entity, children: [] }" />
+
+              <druxt-menu-item
+                v-for="item in children"
+                :key="item.entity.id"
+                :item="item"
+              />
+            </b-nav-item-dropdown>
+          </template>
+        </druxt-menu>
+      </b-col>
+      <b-col>
+        <nuxt />
+      </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+require('dotenv').config()
+
+export default {
+  /*
+   ** Headers of the page
+   */
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+  ],
+
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    '@nuxtjs/pwa',
+    // Doc: https://github.com/nuxt-community/dotenv-module
+    '@nuxtjs/dotenv',
+    // @nuxtjs/proxy module.
+    // https://github.com/nuxt-community/proxy-module
+    '@nuxtjs/proxy',
+    'bootstrap-vue/nuxt',
+    'druxt-breadcrumb',
+    'druxt-entity',
+    'druxt-menu',
+    'druxt-router',
+    'druxt-schema'
+  ],
+
+  /**
+   * Druxt; DRUpal <-> nuXT.
+   */
+  druxt: {
+    baseUrl: process.env.API_URL,
+    auth: {
+      type: 'oauth2',
+      credentials: {
+        clientId: process.env.API_CONSUMER_CLIENT_ID,
+        clientSecret: process.env.API_CONSUMER_CLIENT_SECRET,
+        username: process.env.API_CONSUMER_USERNAME,
+        password: process.env.API_CONSUMER_PASSWORD
+      }
+    },
+    router: {
+      render: 'druxt-entity'
+    }
+  },
+
+  /**
+   * Proxy middleware; https://github.com/nuxt-community/proxy-module.
+   */
+  proxy: {
+    '/sites/default': process.env.API_URL
+  }
+}
