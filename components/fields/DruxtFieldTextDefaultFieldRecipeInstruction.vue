a02c065c96c8383f1fed2bda5cc47cdb4c62d3e9
Stuart Clark <stuart@realityloop.com>
2020-07-14 13:41:47 +1000

    [#3] Fixed Data.

--- components/fields/DruxtFieldTextDefaultFieldRecipeInstruction.vue
+++ components/fields/DruxtFieldTextDefaultFieldRecipeInstruction.vue
@@ -4,7 +4,7 @@
     <!-- <span v-html="items[0].processed" /> -->
     <b-list-group>
       <!-- ^ possibly add flush attribute here -->
-      <b-list-group-item button v-for="item in items" :key="item.message">
+      <b-list-group-item button v-for="item in mock" :key="item.message">
         {{ item.message }}
       </b-list-group-item>
     </b-list-group>
@@ -21,14 +21,14 @@ export default {
 
   computed: {},
 
-  data: {
-    items: [
+  data: () => ({
+    mock: [
       { message: 'Step one.' },
       { message: 'Step two.' },
       { message: 'Step three.' },
       { message: 'Step four.' },
       { message: 'Step five.' },
     ],
-  },
+  }),
 }
 </script>
